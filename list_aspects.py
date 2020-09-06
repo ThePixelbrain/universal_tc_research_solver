5e0e3d1f5185c0ea6f81335c376e1ed28804f81f
Jacek Wikiera <jf@wikiera.net>
2020-09-06 12:21:41 +0200

    ressources organization

--- list_aspects.py
+++
@@ -1,8 +0,0 @@
-import os
-
-#print(os.listdir('./hq_aspect_images_black/thaumcraft'))
-files = os.listdir('./hq_aspect_images_black/thaumcraft')
-string = []
-for file in files:
-	string.append(f"'{file[:-4]}':['','']")
-print(',\n'.join(string))
